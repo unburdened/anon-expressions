55b115365a133bd983f88e5502b7f77c37c6a9df
Unknown <brandonhoffman@gmail.com>
2017-12-17 13:42:17 -0800

    connecting the front end code w/ the backend

--- public/assets/js/expression.js
+++ public/assets/js/expression.js
@@ -1,68 +1,198 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $("#view").on("show.bs.modal", function() {
-    //Send the POST request.
-    $.ajax("/api/expression", {
-      type: "GET"
-    }).then(function(data, status) {
-      console.log("got all expression");
-      console.log(data);
-      console.log(status);
-      // $(".identicon").attr("src", "data:image/png;base64," + data.indenticon);
-      // $("#username-create").html(data.id);
-      // $("#pin-create").html(data.pin);
-      // $("#expression").val("just let yourself go!");
-    });
+// $(function() {
+var locations = [
+  { lat: 33.658676, lng: -117.865189 },
+  { lat: 33.631679, lng: -117.797173 },
+  { lat: 33.676562, lng: -117.743917 },
+  { lat: 33.652574, lng: -117.821889 },
+  { lat: 33.658618, lng: -117.851987 },
+  { lat: 33.631646, lng: -117.757172 },
+  { lat: 33.676577, lng: -117.768121 },
+  { lat: 33.652598, lng: -117.859182 }
+];
+
+var markers = [];
+var map;
+var image = "/assets/img/markerU.png";
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 11,
+    center: { lat: 33.645072, lng: -117.835021 }
   });
+  console.log("inside initmap");
 
-  $("#create").on("show.bs.modal", function() {
-    //Send the POST request.
-    $.ajax("/api/user", {
-      type: "POST"
-    }).then(function(data, status) {
-      console.log("created new user");
-      console.log(data);
-      console.log(status);
-      $(".identicon").attr("src", "data:image/png;base64," + data.indenticon);
-      $("#username-create").html(data.id);
-      $("#pin-create").html(data.pin);
-      $("#expression").val("just let yourself go!");
-    });
-  });
+  infoWindow = new google.maps.InfoWindow();
 
-  $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
 
-    var newMessage = {
-      msg: $("#expression")
-        .val()
-        .trim()
-    };
-
-    var username = $("#username-create").text();
-
-    var lat = getRandomInRange(-180, 180, 6);
-    var lng = getRandomInRange(-180, 180, 6);
-
-    // Send the POST request.
-    $.ajax("/api/expression", {
-      type: "POST",
-      data: {
-        message: newMessage.msg,
-        lat: lat,
-        lng: lng,
-        userId: username
+        infoWindow.setPosition(pos);
+        infoWindow.setContent("Your Location");
+        infoWindow.open(map);
+        map.setCenter(pos);
+        drop();
+      },
+      function() {
+        handleLocationError(true, infoWindow, map.getCenter());
       }
-    }).then(function() {
-      console.log("created new expression");
-      // Reload the page to get the updated list
-      // location.reload();
-    });
+    );
+  } else {
+    // Browser doesn't support Geolocation or user doesn't accept / deny location
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+  // var eventlatLng;
+  google.maps.event.addListener(map, "click", function(event) {
+    addModal();
+    // if()
+    placeMarker(event.latLng);
   });
 
-  function getRandomInRange(from, to, fixed) {
-    return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
-    // .toFixed() returns string, so ' * 1' is a trick to convert to number
+  function placeMarker(location) {
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      icon: image
+    });
+    locations.push(marker);
+    marker.addListener("click", addModal);
   }
+
+  function addModal() {
+    $("#create").modal("show");
+    if ($("#submitExpression") === false) {
+    }
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+function drop() {
+  clearMarkers();
+  for (var i = 0; i < locations.length; i++) {
+    addMarkerWithTimeout(locations[i], i * 100);
+    var markers = locations.map(function(position) {
+      return new google.maps.Marker({
+        position: position,
+        map: map,
+        icon: image
+      });
+    });
+
+    // Add a marker clusterer to manage the markers.
+    var markerCluster = new MarkerClusterer(map, markers, {
+      imagePath:
+        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"
+    });
+  }
+}
+
+function addMarkerWithTimeout(position, timeout) {
+  window.setTimeout(function() {
+    markers.push(
+      new google.maps.Marker({
+        position: position,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        icon: image
+      })
+    );
+  }, timeout);
+}
+
+// var infoWindow = new google.maps.InfoWindow({
+//    content: '<h2> User Post </h2>'
+// });
+// markers.addListener('click', function () {
+//   infoWindow.open(map, markers)
+
+//});
+
+function clearMarkers() {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
+}
+
+function addMarker(cords) {}
+
+$("#view").on("show.bs.modal", function() {
+  //Send the POST request.
+  $.ajax("/api/expression", {
+    type: "GET"
+  }).then(function(data, status) {
+    console.log("got all expression");
+    console.log(data);
+    console.log(status);
+    // $(".identicon").attr("src", "data:image/png;base64," + data.indenticon);
+    // $("#username-create").html(data.id);
+    // $("#pin-create").html(data.pin);
+    // $("#expression").val("just let yourself go!");
+  });
 });
+
+$("#create").on("show.bs.modal", function() {
+  //Send the POST request.
+  $.ajax("/api/user", {
+    type: "POST"
+  }).then(function(data, status) {
+    console.log("created new user");
+    console.log(data);
+    console.log(status);
+    $(".identicon").attr("src", "data:image/png;base64," + data.indenticon);
+    $("#username-create").html(data.id);
+    $("#pin-create").html(data.pin);
+    $("#expression").val("just let yourself go!");
+  });
+});
+
+$(".create-form").on("submit", function(event) {
+  // Make sure to preventDefault on a submit event.
+  event.preventDefault();
+
+  var newMessage = {
+    msg: $("#expression")
+      .val()
+      .trim()
+  };
+
+  var username = $("#username-create").text();
+
+  var lat = getRandomInRange(-180, 180, 6);
+  var lng = getRandomInRange(-180, 180, 6);
+
+  // Send the POST request.
+  $.ajax("/api/expression", {
+    type: "POST",
+    data: {
+      message: newMessage.msg,
+      lat: lat,
+      lng: lng,
+      userId: username
+    }
+  }).then(function() {
+    console.log("created new expression");
+    // Reload the page to get the updated list
+    // location.reload();
+  });
+});
+
+function getRandomInRange(from, to, fixed) {
+  return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
+  // .toFixed() returns string, so ' * 1' is a trick to convert to number
+}
+// });
--- server.js
+++ server.js
@@ -31,8 +31,40 @@ require("./routes/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+  // seedDatabase();
 });
+
+function seedDatabase() {
+  //seed the database w/ 1000 entries
+  const dwGen = require("diceware-generator");
+  const enEFF = require("diceware-wordlist-en-eff");
+  const dwOptions = { language: enEFF, wordcount: 3, format: "string" };
+  const loremIpsum = require("lorem-ipsum");
+
+  for (i = 0; i <= 1000; i++) {
+    const tmp = dwGen(dwOptions);
+    const username = tmp.replace(/\s+/g, "-");
+    const pin = Math.floor(Math.random() * 8999) + 1000;
+    db.User.create({ id: username, pin: pin }).then(function(dbExp) {
+      const lat = Math.floor(Math.random() * 999999) / 1000000 + 33;
+      // console.log(lat);
+      const lng = Math.floor(Math.random() * 999999) / 1000000 - 118;
+      // console.log(lng);
+      const newMessage = loremIpsum({
+        count: 1,
+        units: "sentences"
+      });
+      // console.log(newMessage);
+      db.Expression.create({
+        message: newMessage,
+        lat: lat,
+        lng: lng,
+        UserId: username
+      });
+    });
+  }
+}
